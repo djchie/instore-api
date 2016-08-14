f67c816492191490ae1d8cddca2668faf2111f6e
Derrick <djchie.dev@gmail.com>
2016-08-14 03:10:08 -0700

    Sets up initial api router

--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
 const Koa = require('koa');
+const Router = require('koa-router');
+
+const port = process.env.PORT || 5000;
+const apiVersion = '1.0';
+
 const server = new Koa();
+const router = new Router({
+  prefix: '/api/' + apiVersion,
+});
 
 const logger = require('koa-logger');
 const responseTime = require('koa-response-time');
-
-const port = process.env.PORT || 5000;
 
 // Logs requests made and reponses sent
 server.use(logger());
@@ -13,9 +19,15 @@ server.use(logger());
 server.use(responseTime());
 
 // response middleware
-server.use(ctx => {
+router.get('/', async (ctx, next) => {
   ctx.body = 'Hello World';
 });
+
+router.get('/search', async (ctx, next) => {
+  ctx.body = 'You\'re about to make a search!';
+});
+
+server.use(router.routes());
 
 server.listen(port, () => console.log('Running at http://localhost:' + port));
 
