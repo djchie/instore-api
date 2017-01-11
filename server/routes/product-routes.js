4d820c180d31d7ed718e09e9d1ac2b08163293db
Derrick <djchie.dev@gmail.com>
2017-01-11 10:38:53 -0800

    Implements rudimentary product endpoint to get all products

--- server/routes/product-routes.js
+++ server/routes/product-routes.js
@@ -7,7 +7,9 @@ const Product = database.models.Product;
 const productRouter = new Router();
 
 productRouter.get('/product', async (ctx, next) => {
-  ctx.body = 'You\'re on a product detail page!';
+  const products = await Product.findAll()
+  // ctx.body = 'You\'re on a product detail page!';
+  ctx.body = products;
 });
 
 productRouter.post('/product'), async (ctx, next) => {
